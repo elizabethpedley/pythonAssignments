498a68ca1cd8fe65e90ad4d2d8da7149c5e05606
Liz <elizabethmpedley@gmail.com>
2018-03-31 17:39:14 -0500

    created multiples, sum, average assignment

---
+++ multiplesSumAverage.py
@@ -0,0 +1,11 @@
+#code that prints all the odd numbers from 1 to 1000
+for i in range(1,1000,2):
+    print i 
+# print all the multiples of 5 from 5 to 1,000,000
+for i in range(5,1000001,5):
+    print i
+# print the sum of all the values in the list
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+# print the average of the values in the list
+print (sum(a)/len(a))

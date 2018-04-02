083c95c6ac225d81e3755074f160c030c6917086
Liz <elizabethmpedley@gmail.com>
2018-04-02 14:18:50 -0500

    created tuples out assignment

---
+++ dictionary_tuples.py
@@ -0,0 +1,10 @@
+def tuples_out(dict):
+    return dict.items()
+
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+print tuples_out(my_dict)
